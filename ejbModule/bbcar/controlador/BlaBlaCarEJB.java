befd2e9dc1a39e17ca685fbb56de8dfb186fe297
JosePina98 <jose_pina_98@hotmail.com>
2019-12-21 19:02:24 +0100

    relleno el blablacarRemote

--- ejbModule/bbcar/controlador/BlaBlaCarEJB.java
+++ ejbModule/bbcar/controlador/BlaBlaCarEJB.java
@@ -1,22 +1,27 @@
 package bbcar.controlador;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.SessionContext;
-import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 
 import bbcar.dao.DAOException;
 import bbcar.dao.DAOFactoria;
 import bbcar.dao.interfaces.DAOFactoriaLocal;
+import bbcar.modelo.Coche;
+import bbcar.modelo.Municipio;
+import bbcar.modelo.Provincia;
+import bbcar.modelo.Reserva;
+import bbcar.modelo.Usuario;
+import bbcar.modelo.Valoracion;
+import bbcar.modelo.Viaje;
 
 @Stateless(name = "BlaBlaCarRemoto")
 public class BlaBlaCarEJB implements BlaBlaCarRemote {
-	@EJB(beanName = "Contador")
-	private ContadorEJB contador;
 	
 	@EJB(beanName="Factoria")
 	private DAOFactoriaLocal factoria;
@@ -37,16 +42,229 @@ public class BlaBlaCarEJB implements BlaBlaCarRemote {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
-	public boolean login(String usuario, String clave) {
+	public boolean registrarUsuario(String usuario, String clave, Date fecha_nacimiento, String profesion, String email,
+			String nombre, String apellidos) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean registrarUsuario(String usuario, String clave, Date fecha_nacimiento, String profesion, String email,
-			String nombre, String apellidos) {
+	public int login(String usuario, String clave) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addCoche(String matricula, String modelo, Integer anyo, Integer confort) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Integer registrarViaje(Integer plazas, Double precio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer registrarParadaOrigen(Integer idViaje, String ciudad, String calle, Integer numero, Integer CP,
+			Date fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer registrarParadaDestino(Integer idViaje, String ciudad, String calle, Integer numero, Integer CP,
+			Date fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer reservarViaje(Integer idViaje, String comentario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int aceptarReserva(Reserva reserva) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void rechazarReserva(Reserva reserva) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int valorarUnPasajero(Integer idViaje, Integer idUsuarioPasajero, String comentario, Integer puntuacion) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int valorarUnConductor(Integer idViaje, Integer idUsuarioConductor, String comentario, Integer puntuacion) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Viaje> buscarViajesLazy(String municipioOrigen, String municipioDestino, Date fechaHora, int start,
+			int max) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer countViajes(String ciudadOrigen, String ciudadDestino, Date fechaHora) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Coche getCocheUsuario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsuarioLogueado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCocheRegistrado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void actualizarCoche(Integer anyo, String matricula, String modelo, Integer nivelConfort) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Provincia> getProvincias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Municipio> getMunicipios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getMunicipios(Provincia p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getMunicipios(String nombreProvincia) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarViaje(Integer idViaje) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Usuario> getPasajeros(Integer idViaje) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getIdConductor(Integer idViaje) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Viaje> getViajesConductorRealizados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Viaje> getViajesConductorPendientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValoracionPasajeroHecha(Integer idViaje, Integer idPasajero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Viaje> getViajesPasajeroRealizados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Viaje> getViajesPasajeroPendientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValoradoConductor(Integer idViaje, Integer idConductor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean existeUsuario(String usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isReservaPendiente(Integer idReserva) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Valoracion> getValoracionesRealizadas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Valoracion> getValoracionesRecibidas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reserva> getReservas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rechazarOtrasReservas(Integer idViaje) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean existeMatricula(String matricula) {
 		// TODO Auto-generated method stub
 		return false;
 	}
--- ejbModule/bbcar/controlador/BlaBlaCarRemote.java
+++ ejbModule/bbcar/controlador/BlaBlaCarRemote.java
@@ -1,17 +1,97 @@
 package bbcar.controlador;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Remote;
 
 import bbcar.dao.interfaces.DAOFactoriaLocal;
+import bbcar.modelo.Coche;
+import bbcar.modelo.Municipio;
+import bbcar.modelo.Provincia;
+import bbcar.modelo.Reserva;
+import bbcar.modelo.Usuario;
+import bbcar.modelo.Valoracion;
+import bbcar.modelo.Viaje;
 
 @Remote
 public interface BlaBlaCarRemote {
 	public DAOFactoriaLocal getFactoria();
-	public boolean login(String usuario, String clave);
-	public boolean registrarUsuario(String usuario, String clave, Date fecha_nacimiento, 
-			String profesion, String email, String nombre, String apellidos);
+	
+	public boolean registrarUsuario(String usuario, String clave, Date fecha_nacimiento, String profesion, String email,
+			String nombre, String apellidos);
+	
+	public int login(String usuario, String clave);
+	
+	public int addCoche(String matricula, String modelo, Integer anyo, Integer confort);
 
-	//Falta declarar el resto de metodos
+	public Integer registrarViaje(Integer plazas, Double precio);
+	
+	public Integer registrarParadaOrigen(Integer idViaje, String ciudad, String calle, Integer numero,
+			Integer CP, Date fecha);
+	
+	public Integer registrarParadaDestino(Integer idViaje, String ciudad, String calle, Integer numero,
+			Integer CP, Date fecha);
+
+	public Integer reservarViaje(Integer idViaje, String comentario);
+
+	public int aceptarReserva(Reserva reserva);
+
+	public void rechazarReserva(Reserva reserva);
+
+	public int valorarUnPasajero(Integer idViaje, Integer idUsuarioPasajero, String comentario, Integer puntuacion);
+
+	public int valorarUnConductor(Integer idViaje, Integer idUsuarioConductor, String comentario, Integer puntuacion);
+	
+	public List<Viaje> buscarViajesLazy(String municipioOrigen, String municipioDestino, Date fechaHora, int start, int max);
+	
+	public Integer countViajes(String ciudadOrigen, String ciudadDestino, Date fechaHora);
+	
+	public Coche getCocheUsuario();
+
+	public boolean isUsuarioLogueado();
+
+	public boolean isCocheRegistrado();
+
+	public void actualizarCoche(Integer anyo, String matricula, String modelo, Integer nivelConfort);
+
+	public List<Provincia> getProvincias();
+	
+	public List<Municipio> getMunicipios();
+
+	public List<String> getMunicipios(Provincia p);
+	
+	public List<String> getMunicipios(String nombreProvincia);
+
+	public void eliminarViaje(Integer idViaje);
+
+	public List<Usuario> getPasajeros(Integer idViaje);
+
+	public Integer getIdConductor(Integer idViaje);
+
+	public List<Viaje> getViajesConductorRealizados();
+
+	public List<Viaje> getViajesConductorPendientes();
+
+	public boolean isValoracionPasajeroHecha(Integer idViaje, Integer idPasajero);
+
+	public List<Viaje> getViajesPasajeroRealizados();
+
+	public List<Viaje> getViajesPasajeroPendientes();
+
+	public boolean isValoradoConductor(Integer idViaje, Integer idConductor);
+
+	public boolean existeUsuario(String usuario);
+
+	public boolean isReservaPendiente(Integer idReserva);
+
+	public List<Valoracion> getValoracionesRealizadas();
+
+	public List<Valoracion> getValoracionesRecibidas();
+
+	public List<Reserva> getReservas(); 
+
+	public void rechazarOtrasReservas(Integer idViaje);
+	
+	public boolean existeMatricula(String matricula);
 }
